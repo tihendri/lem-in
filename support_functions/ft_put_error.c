df7bdb7efbe2978a386dcc3cd2e0263627fdc4cc
tihendri <tihendri@student.wethinkcode.co.za>
2020-05-11 22:59:29 +0200

    updated to prevent memory leaks

--- support_functions/ft_put_error.c
+++ support_functions/ft_put_error.c
@@ -1,7 +1,15 @@
 #include "../lemin.h"
 
-void		ft_put_error(void)
+void		ft_put_error()
 {
+	ft_putstr("\033[1;31mERROR\n");
+	exit(-1);
+}
+
+void		ft_put_error_only_lemin(t_lemin *lemin)
+{
+	if (lemin)
+		free(lemin);
 	ft_putstr("\033[1;31mERROR\n");
 	exit(-1);
 }
