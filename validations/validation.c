a2a7e2a2c8d470f650dd89182e9bf83b66882395
tihendri <tihendri@student.wethinkcode.co.za>
2020-05-11 23:00:50 +0200

    updated to prevent memory leaks

--- validations/validation.c
+++ validations/validation.c
@@ -3,26 +3,28 @@
 int		validation(char *line, t_lemin *lemin)
 {
 	char	*map;
+	char	*line_read;
 
-	map = ft_strdup("");
+	map = "\0";
 	while (get_next_line(0, &line) && line)
 	{
-		map = ft_join_free(map, ft_strjoin(line, "\n"), BOTH);
+		line_read = line;
+		free(line);
+		map = ft_join_free(map, ft_strjoin(line_read, "\n"), BOTH);
 		lemin->map = map;
-		if (!lemin->count_ants && check_ants(line, lemin))
+		free(map);
+		if (!lemin->count_ants && check_ants(line_read, lemin))
 			;
-		else if (check_commands(line, lemin) || check_comments(line))
+		else if (check_commands(line_read, lemin) || check_comments(line_read))
 			;
-		else if (check_spaces(line) && check_room_coordinates(line))
-			parse_rooms(line, lemin);
-		else if (check_tunnels(line) && check_tunnels_safe(line))
-			parse_tunnels(line, lemin);
-		else if (check_line_break(line))
+		else if (check_spaces(line_read) && check_room_coordinates(line_read))
+			parse_rooms(line_read, lemin);
+		else if (check_tunnels(line_read) && check_tunnels_safe(line_read))
+			parse_tunnels(line_read, lemin);
+		else if (check_line_break(line_read))
 			break ;
 		else
 			return (0);
-		if (line[0] != '\n' && line[1] != '\n')
-			free(line);
 	}
 	if (!check_change_tunnel(lemin) || !bad_rooms(&lemin) || empty_struct(&lemin))
 		return (0);
