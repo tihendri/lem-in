5112b7f4a8865d9276c96f4ca4d04bcc6ada45f6
timothy <tihendri@student.wethinkcode.co.za>
2020-05-12 14:49:10 +0200

    reverted back to old layout, free conflicts

--- support_functions/ft_put_error.c
+++ support_functions/ft_put_error.c
@@ -5,11 +5,3 @@ void		ft_put_error()
 	ft_putstr("\033[1;31mERROR\n");
 	exit(-1);
 }
-
-void		ft_put_error_only_lemin(t_lemin *lemin)
-{
-	if (lemin)
-		free(lemin);
-	ft_putstr("\033[1;31mERROR\n");
-	exit(-1);
-}
