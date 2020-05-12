f608baf57bd1072954dd7a756e40e3979039522f
timothy <tihendri@student.wethinkcode.co.za>
2020-05-12 14:49:23 +0200

    reverted back to old layout, free conflicts

--- validations/validation.c
+++ validations/validation.c
@@ -3,25 +3,21 @@
 int		validation(char *line, t_lemin *lemin)
 {
 	char	*map;
-	char	*line_read;
 
-	map = "\0";
+	map = ft_strdup("");
 	while (get_next_line(0, &line) && line)
 	{
-		line_read = line;
-		free(line);
-		map = ft_join_free(map, ft_strjoin(line_read, "\n"), BOTH);
+		map = ft_join_free(map, ft_strjoin(line, "\n"), BOTH);
 		lemin->map = map;
-		free(map);
-		if (!lemin->count_ants && check_ants(line_read, lemin))
+		if (!lemin->count_ants && check_ants(line, lemin))
 			;
-		else if (check_commands(line_read, lemin) || check_comments(line_read))
+		else if (check_commands(line, lemin) || check_comments(line))
 			;
-		else if (check_spaces(line_read) && check_room_coordinates(line_read))
-			parse_rooms(line_read, lemin);
-		else if (check_tunnels(line_read) && check_tunnels_safe(line_read))
-			parse_tunnels(line_read, lemin);
-		else if (check_line_break(line_read))
+		else if (check_spaces(line) && check_room_coordinates(line))
+			parse_rooms(line, lemin);
+		else if (check_tunnels(line) && check_tunnels_safe(line))
+			parse_tunnels(line, lemin);
+		else if (check_line_break(line))
 			break ;
 		else
 			return (0);
