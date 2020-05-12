d62a0e1ff44ae7570e900a9cde73f7c70dc19f5a
timothy <tihendri@student.wethinkcode.co.za>
2020-05-12 14:48:49 +0200

    reverted back to old layout, free conflicts

--- algorithm/parse_rooms.c
+++ algorithm/parse_rooms.c
@@ -19,9 +19,8 @@ static void	push_other_rooms(t_lemin **lemin, t_rooms *room)
 			rooms[i] = temp->rooms[i];
 		i++;
 	}
-	// free(temp->rooms);
+	free(temp->rooms);
 	temp->rooms = rooms;
-	free(rooms);
 }
 
 int			parse_rooms(char *line, t_lemin *lemin)
@@ -49,8 +48,6 @@ int			parse_rooms(char *line, t_lemin *lemin)
 		lemin->start_room = 1;
 	}
 	ft_free_str(str);
-	free(room->title);
-	free(room);
 	lemin->count_rooms++;
 	return (1);
 }
